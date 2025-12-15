6f615f71d3527e5e48735939f960ec8162f4a712
Avilash Ghosh <58250550+Avilash2001@users.noreply.github.com>
2025-12-15 11:07:03 +0530

    added layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,11 @@
+import { Metadata } from "next";
 import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Black Ink",
+  description: "A AI story adventure game",
+};
+
 
 export default function RootLayout({
   children,
